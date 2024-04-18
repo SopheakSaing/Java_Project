bad61ffe871999d8d6e7ca4689bd238ac70526de
Sopheak Saing <sopheak.saing@khalibre.com>
2024-04-18 17:06:11 +0700

    ArrayList explained

---
+++ src/school/management/system/deleteMain.java
@@ -0,0 +1,20 @@
+package src.school.management.system;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class deleteMain {
+
+  public static void main(String[] args) {
+
+    List<Integer> numbers = new ArrayList<>();
+    numbers.add(5);
+    numbers.add(20);
+    numbers.add(10);
+    numbers.add(52);
+    numbers.add(15);
+    System.out.println(numbers);
+  }
+
+}
